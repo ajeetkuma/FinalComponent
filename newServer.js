a47ec04ca2d6fedf37e797b34e9b04322ea2ebf9
ajeetkuma <ajeet.kumar6171@gmail.com>
2017-06-20 20:50:28 +0530

    first committthhhh

--- newServer.js
+++ newServer.js
@@ -11,16 +11,13 @@ var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 
 dotenv.load({ path: '.env.example' });
-/*
-Whole salesforce code
- */
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -55,11 +52,15 @@ function handleError(res, reason, message, code) {
 
 app.post("/getRecords", function(req, res) {
   console.log('geetting data');
-  console.log(req.body);
+  console.log(req.b);
+  console.log('geetting paa');
+  console.log(req.param["data"]);
   var fields=req.body;
-  
+  var x= fields["data"];
+  console.log('xxx');
+  console.log(x);
   setTimeout(function() {
-    var x= fields["data"];
+    
     var obj='Account';
     
       var allExistingData =[];
@@ -77,7 +78,7 @@ app.post("/getRecords", function(req, res) {
                 console.log('results length');
                 console.log(allExistingData.length);
                 if(allExistingData.length > 0)
-                  fields.forEach(function(doc) {
+                  x.forEach(function(doc) {
                     db.collection(obj).update(
                        {Id:doc.Id},
                        doc,
@@ -87,7 +88,7 @@ app.post("/getRecords", function(req, res) {
                 });
                 else{
                   console.log('inside else');
-                  db.collection(obj).insert(fields, function(err, doc) {
+                  db.collection(obj).insert(x, function(err, doc) {
                     if (err) {
                       handleError(res, err.message, "Failed to create new contact.");
                     } else {
@@ -107,3 +108,49 @@ app.post("/getRecords", function(req, res) {
 
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const express = require('express')
+const bodyParser = require('body-parser')
+ 
+// Create a new instance of express
+const app = express()
+ app.use(bodyParser.json()); 
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// Route that receives a POST request to /sms
+app.post('/sms', function (req, res) {
+  const body = req.body;
+  console.log('xvxv');
+  console.log(body);
+  res.set('Content-Type', 'text/plain')
+  res.send(`You sent: ${body} to Express`)
+})
+ 
+// Tell our app to listen on port 3000
+app.listen(3000, function (err) {
+  if (err) {
+    throw err
+  }
+ 
+  console.log('Server started on port 3000')
+})*/
