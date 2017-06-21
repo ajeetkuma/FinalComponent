ee7dc73f4f35414ff42c4061eacd8c1821b1fd12
ajeetkuma <ajeet.kumar6171@gmail.com>
2017-06-21 11:25:10 +0530

    sddsfs

--- newServer.js
+++ newServer.js
@@ -62,6 +62,11 @@ app.get("/hello",function(req, res) {
 app.get("/helloworld",function(req, res) {
   res.send('hello fuckingworld xxxxxx');
 })
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.post("/getRecords", function(req, res) {
   console.log('geetting data');
   
@@ -105,7 +110,7 @@ app.post("/getRecords", function(req, res) {
                     if (err) {
                       handleError(res, err.message, "Failed to create new contact.");
                     } else {
-                      //res.status(201).json(doc);
+                      //sres.status(201).json(doc);
                     }
                   });
                 }
