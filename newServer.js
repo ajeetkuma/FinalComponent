49d4128bd550654e1634641731dc93e5514c343a
ajeetkuma <ajeet.kumar6171@gmail.com>
2017-06-22 12:32:05 +0530

    ajakakakaffg

--- newServer.js
+++ newServer.js
@@ -73,6 +73,7 @@ app.all('/*', function(req, res, next) {
 app.post("/getRecords", function(req, res,next) {
   console.log('req');
   console.log('req');
+  console.log('req');
   console.log(req);
   var fields=req.body;
   console.log('fields');
